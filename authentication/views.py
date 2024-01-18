2d96880cff5859cf9781557d8cc8f646fd6a8a4d
DevHouda <devhouda@gmail.com>
2024-01-18 16:54:39 +0100

    User model and Login page with class-based views

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from authentication import forms
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+def login_page(request):
+    message = ''
+    form = forms.LoginForm()
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                message = 'Login failed!'
+    return render(request, 'authentication/login.html', context={'form': form, 'message': message})
--- photoblog/settings.py
+++ photoblog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authetication',
+    'authentication',
     'blog',
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'photoblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.joinpath('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.User'
+LOGIN_URL = 'login'
